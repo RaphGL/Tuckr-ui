70c2af2d1be7c76517ca9fd13f500b677d6cd732
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2024-09-15 21:17:45 -0400

    Fix window config

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,17 @@ fn main() -> eframe::Result {
 	let native_options = eframe::NativeOptions {
 		viewport: egui::ViewportBuilder::default()
 			.with_transparent(true)
-			.with_inner_size([500.0, 460.0])
-			.with_min_inner_size([500.0, 420.0])
+			.with_inner_size([600.0, 540.0])
+			.with_min_inner_size([590.0, 520.0])
 			.with_icon(
-				// NOTE: Adding an icon is optional
+				// Icon
 				eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
 					.expect("Failed to load icon"),
 			),
 		..Default::default()
 	};
 	eframe::run_native(
-		"eframe template",
+		"Tuckr UI",
 		native_options,
 		Box::new(|cc| {
 			// This gives us image support:
